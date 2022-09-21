121d3d3370025bd9d9e91cee9c864e9d8dd0917f
Julie Park <juliepark242@gmail.com>
2022-09-21 21:09:44 +0100

    Set up post feed

--- social_media_backend/urls.py
+++ social_media_backend/urls.py
@@ -20,9 +20,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # include urls in the app core
     path('', include('core.urls'))
 ]
 
-urlpatters = urlpatterns+static(settings.MEDIA_URL, 
+urlpatterns = urlpatterns+static(settings.MEDIA_URL,
 document_root=settings.MEDIA_ROOT)
